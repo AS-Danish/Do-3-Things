30e51a16bbc4c1b0d70f7c5168a26caa59bbbe59
AS-Danish <asdanish123@gmail.com>
2025-10-19 20:38:49 +0530

    Initiazlied the Storage Path and Open the Box Hive

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,23 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:path_provider/path_provider.dart';
 import 'firebase_options.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
+  final dir = await getApplicationDocumentsDirectory();
+  Hive.init(dir.path);
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await Hive.initFlutter();
   Hive.registerAdapter(TaskAdapter());
+
+  await Hive.openBox<Task>('tasksBox');
+
   runApp(const MyApp());
 }
 
